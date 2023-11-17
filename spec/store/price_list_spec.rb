d4b6792815d922e7b8cf5890d48745acf382ca23
Sergei O. Udalov <udalov.x@mail.ru>
2023-11-17 08:03:17 +0300

    patch_02 08:03

--- spec/store/price_list_spec.rb
+++ spec/store/price_list_spec.rb
@@ -7,8 +7,6 @@ module Store
     context do
       let(:lines) do
         <<~LINES
-          Price#{' '}
-
           +79151274685
                                🔥🔥🔥🔥🔥
            (Звоните принесём на павильон)
@@ -26,6 +24,26 @@ module Store
       it { expect(goods.first.device).to eq "Apple Technology" }
       it { expect(goods.first.model).to eq "SE 3 128" }
       it { expect(goods.first.cost).to eq 40_500 }
+    end
+
+    context do
+      let(:lines) do
+        <<~LINES
+                                💥iPhone 15💥
+
+          🇯🇵📱*15P 256 Natural-131000
+
+          🇯🇵📱*15PM 256🔵-142000
+
+          🇯🇵📱*15PM 512⚫-163000
+          🇯🇵📱*15PM 512🔵-163500
+        LINES
+      end
+
+      it { expect(goods.entries.size).to eq 2 }
+      it { expect(goods.first.device).to eq "iPhone 15" }
+      it { expect(goods.first.model).to eq "15P 256 Natural" }
+      it { expect(goods.first.cost).to eq 131000 }
     end
   end
 end
